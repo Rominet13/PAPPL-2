a62a1aafdf46dcac393b2afbe1da5499d66fa5df
Rominet13 <volphoenix@outlook.fr>
2015-12-29 20:06:43 +0100

    Initial commit (1ère version avec 3ième méthode)

---
+++ src/conversionanjson/ConversionAnJson.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conversionanjson;
+
+/**
+ *
+ * @author volph
+ */
+public class ConversionAnJson {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AnToJson.methode("circlock");
+//System.out.println("11\t22\n3-3\r44\f55\r6");
+    }
+    
+}
