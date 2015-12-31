0cef4a66fff8979e46293027dee90b48e7849076
Rominet13 <volphoenix@outlook.fr>
2015-12-31 13:14:25 +0100

    test et maitrise du StringBuffer.

--- src/conversionanjson/ConversionAnJson.java
+++ src/conversionanjson/ConversionAnJson.java
@@ -18,7 +18,19 @@ public class ConversionAnJson {
         AnToJson.methode("circlock");
 //System.out.println("11\t22\n3-3\r44\f55\r6");
 //StringBuffer a1 = new StringBuffer();
-//a1.append("salut"); //mieux permet le delete à tel position...
+//a1.append("salut, et toi, ca va,"); //mieux permet le delete à tel position...
+//System.out.println(a1.lastIndexOf(","));
+//System.out.println(a1.length());
+//        System.out.println(a1);
+//        a1.deleteCharAt(20);
+//        System.out.println(a1);
     }
     
 }
+/* **** String   VS   StringBuffer ******
+StringBuffer a1 = new StringBuffer();
+a1.append("salut, et toi, ca va,");
+a1.lastIndexOf(",")
+a1.length());
+a1.deleteCharAt(20);
+*/
