3b1ffa9d68bca74858db795c1db12bfcb456b139
Rominet13 <volphoenix@outlook.fr>
2016-01-18 19:01:21 +0100

    Rien juste changement de nom de fichier à traiter.

--- src/conversionanjson/ConversionAnJson.java
+++ src/conversionanjson/ConversionAnJson.java
@@ -15,7 +15,7 @@ public class ConversionAnJson {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        AnToJson.methode("testcom");
+        AnToJson.methode("circlock");
         
 //System.out.println("11\t22\n3-3\r44\f55\r6");
 //StringBuffer a1 = new StringBuffer();
