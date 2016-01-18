09b88da01579878cf366cf8903fdfd1237a8cfd1
Rominet13 <volphoenix@outlook.fr>
2016-01-18 17:19:39 +0100

    rien...

--- src/conversionanjson/ConversionAnJson.java
+++ src/conversionanjson/ConversionAnJson.java
@@ -15,7 +15,7 @@ public class ConversionAnJson {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        AnToJson.methode("ERBB_G1-S");
+        AnToJson.methode("testcom");
         
 //System.out.println("11\t22\n3-3\r44\f55\r6");
 //StringBuffer a1 = new StringBuffer();
