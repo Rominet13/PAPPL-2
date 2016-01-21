3815577fbb95027e08b25dc492db374a115e0c02
Rominet13 <volphoenix@outlook.fr>
2016-01-21 21:16:34 +0100

    commit de push (juste pour les nouveaux fichiers créés par l'appli

--- src/conversionanjson/ConversionAnJson.java
+++ src/conversionanjson/ConversionAnJson.java
@@ -15,7 +15,7 @@ public class ConversionAnJson {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        AnToJson.methode("circlock");
+        AnToJson.methode("egfr104");
         
 //System.out.println("11\t22\n3-3\r44\f55\r6");
 //StringBuffer a1 = new StringBuffer();
